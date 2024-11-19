d8ab9f414b8b1d9131d7436f0bf219de1622e36e
aygalic <aygalic@hotmail.fr>
2024-11-19 17:13:36 +0100

    feat: proper preprocessor class for outlier removal

---
+++ utils/pre_processor/outlier_remover.py
@@ -0,0 +1,25 @@
+
+from .pre_processor import PreProcessorABC
+from collections import Counter
+
+import pandas as pd
+
+class OutlierRemover(PreProcessorABC):
+    def __init__(self):
+        self.features_to_process = {}
+        self.threshold = 10
+
+    def fit(self, data: pd.DataFrame) -> None:
+        for col in data:
+            if data[col].dtype == 'object':
+
+                self.features_to_process[col] = [var for (var,count) in Counter(data[col].astype(str)).items() if count > 10]
+
+    
+    def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+        for col in data:
+            if data[col].dtype == 'object':
+
+                data[col] = [var if var in self.features_to_process[col] else "Unknown" for var in data[col]]
+
+        return data
